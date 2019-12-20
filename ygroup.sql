ca39ce9720884afdf0ee433cf90f8b486ec415f1
sad <harshitsharma0003@gmail.com>
2019-12-20 20:56:39 +0530

    Add existing file1

--- ygroup.sql
+++ ygroup.sql
@@ -1,21 +1,18 @@
-CREATE TABLE `CacheTable` (
-  `ML_Key` varchar(255) DEFAULT NULL,
-  `ML_Value` double DEFAULT NULL,
-  `calendar_yearmonth` int(25) DEFAULT NULL
-) 
+
+
+DROP TABLE sku_mapping;
+
+DROP TABLE pipo;
+
+DROP TABLE filters;
+DROP TABLE users;
 
 
 
 
-CREATE TABLE `UserLog` (
-  `Username` varchar(25) NOT NULL,
-  `activity` varchar(25) NOT NULL,
-  `datetimestamp` varchar(75) NOT NULL
-) 
 
 
-
-CREATE TABLE `filter_data` (
+CREATE TABLE `filters` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(100) NOT NULL,
   `filter_name` varchar(100) NOT NULL,
@@ -24,21 +21,21 @@ CREATE TABLE `filter_data` (
   `sku` longtext NOT NULL,
   `valuedefault` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
+);
 
 
 
-CREATE TABLE `pipo_mapping_updated` (
+CREATE TABLE `pipo` (
   `fromid` int(10) DEFAULT NULL,
   `toid` int(10) DEFAULT NULL,
   `fromweek` int(10) DEFAULT NULL,
   `fgid` varchar(75) DEFAULT NULL,
   `state` varchar(75) DEFAULT NULL
-) 
+);
 
 
 
-CREATE TABLE `pipo_final` (
+CREATE TABLE `sku_mapping` (
   `material` varchar(10) DEFAULT NULL,
   `Sku` varchar(75) DEFAULT NULL,
   `Forecastinggroup` varchar(100) DEFAULT NULL,
@@ -46,31 +43,10 @@ CREATE TABLE `pipo_final` (
   `minimum` int(30) DEFAULT NULL,
   `maximum` int(30) DEFAULT NULL,
   `prime` varchar(75) DEFAULT 'PRIMARY'
-) 
+);
 
 
 
-
-
-CREATE TABLE `plan_data` (
-  `Pk_Combination` varchar(255) NOT NULL,
-  `Sku` varchar(255) NOT NULL,
-  `cpg` varchar(255) NOT NULL,
-  `plant` varchar(255) NOT NULL,
-  `Calendar_Week` int(11) NOT NULL,
-  `fva` double NOT NULL,
-  `Final_Forecast` double DEFAULT NULL,
-  `user` varchar(100) NOT NULL,
-  `comments1` varchar(255) DEFAULT NULL,
-  `comments2` varchar(255) DEFAULT NULL,
-  `Temp_Value` tinyint(1) DEFAULT NULL,
-  `Final_Forecast_Temp` double DEFAULT NULL,
-  `calendar_yearmonth` int(25) NOT NULL,
-  `ml` double(30,2) DEFAULT NULL,
-  `id` int(30) DEFAULT NULL,
-  `lockcell` varchar(5) DEFAULT NULL,
-  `forecasting` varchar(75) DEFAULT NULL
-) 
 
 
 
@@ -78,6 +54,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(255) DEFAULT NULL,
   `horizon` int(30) DEFAULT NULL,
+  `region` int(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
+);
 
